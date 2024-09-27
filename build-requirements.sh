55047542f7ba61bae7c43aad89b5c6311f4a21db
David Kinzer <dtkinzer@gmail.com>
2024-09-27 08:38:49 -0400

    Add description and make exported names less generic.

---
+++ build-requirements.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+##
+## This script is used to build a requirements.txt from Pipfile and airflow constraints.
+## This is a necessary step when trying to rebuild a Pipfile while applying published 
+## airflow constraints.
+##
+
+# Get Airflow version from Pipfile
+AIRFLOW_VERSION=$(pipenv run pip freeze | grep 'apache-airflow==' | cut -d'=' -f3)
+echo "Airflow version: $AIRFLOW_VERSION"
+
+# Get Python version from .python-version (first two parts)
+PYTHON_VERSION=$(cut -d'.' -f1-2 .python-version)
+echo "Python version: $PYTHON_VERSION"
+
+# Download the Airflow constraints file
+curl -O https://raw.githubusercontent.com/apache/airflow/constraints-$AIRFLOW_VERSION/constraints-$PYTHON_VERSION.txt
+
+
+# Generate the list of packages from the Pipfile
+packages=$(grep '\w\s*=\s*"==' Pipfile | cut -d= -f1 | sed 's/[[:space:]]*$/==/')
+
+# Create a new empty requirements.txt file
+> pipfile-requirements.txt
+
+# Loop through each package and check if it's in the constraints file
+while IFS= read -r package; do
+  match=$(grep "^$package" "constraints-$PYTHON_VERSION.txt")
+  if [ "$match" != "" ]; then
+    echo $match >> pipfile-requirements.txt;
+  fi
+done <<< "$packages"
