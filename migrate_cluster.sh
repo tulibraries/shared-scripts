aaeb72119366de8e7c6c6e42699e0d5545e95b97
David Kinzer <dtkinzer@gmail.com>
2025-04-09 09:18:35 -0400

    Check that given BACKUP_NAME exists.

--- migrate_cluster.sh
+++ migrate_cluster.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+
+# This script is used to migrate from one cluster to another.
+# It assumes that we are migrating from the same namespace.
+#
+# This script was tested using the following dependency versions
+# GNU bash, version 5.2.37
+# velero, version v1.15.2
+# jq, version jq-1.7.1
+
 set -e
 : "${DEBUG:=false}"
 
@@ -6,23 +15,26 @@ if [ $DEBUG = "true" ]; then
 	set -x
 fi
 
-: "${CLUSTER_NAME:=prod-library}"
+: "${CLUSTER_NAME:=dev-library}"
 : "${FROM_CLUSTER_ID:=0}"
 : "${TO_CLUSTER_ID:=1}"
 : "${NAMESPACE:=$1}"
 : "${EXCLUDE_RESOURCES:='certificaterequests.cert-manager.io,orders.acme.cert-manager.io,certificates.cert-manager.io,apps.catalog.cattle.io,configauditreports.aquasecurity.github.io,exposedsecretreports.aquasecurity.github.io,sbomreports.aquasecurity.github.io,vulnerabilityreports.aquasecurity.github.io'}"
+# If BACKUP_NAME is not given, a new backup will be generated.
+: "${BACKUP_NAME:=}"
 
-# Switch context to cluster and namespace we are migrating from
+# Switch context to cluster and namespace we are migrating from.
 kubectl config use ${CLUSTER_NAME}${FROM_CLUSTER_ID}-fqdn
 kubectl get namespace ${NAMESPACE}
 kubectl config set-context --current --namespace $NAMESPACE
 
 # We will be relaxing the namespace privilages,
-# but we want to keep original value so can reset it later.
+# but we want to keep the original value to reset it later.
 echo
 echo "Save the original pod security settings..."
 ORIG_PRIVILAGES=$(kubectl get namespace $NAMESPACE -o jsonpath="{.metadata.labels.pod-security\.kubernetes\.io/enforce}")
 
+# Process for creating a new backup of given NAMESPACE in the current context.
 create_backup() {
 	echo
 	echo "Backing up $NAMESPACE from $CLUSTER_NAME-$FROM_CLUSTER_ID"
@@ -56,11 +68,15 @@ create_backup() {
 	done
 }
 
-# only create backup if we don't provide one.
+# Only create a backup if we don't provide one.
 if [ -z "${BACKUP_NAME:-}" ]; then
 	create_backup
+else
+	# Make sure that given BACKUP_NAME exists.
+	BACKUP_NAME=$(velero backup describe $BACKUP_NAME --output json | jq -r '.metadata.name')
 fi
 
+# Switch context to cluster we are migrating to.
 kubectl config use ${CLUSTER_NAME}${TO_CLUSTER_ID}-fqdn
 kubectl get namespace ${NAMESPACE}
 kubectl config set-context --current --namespace $NAMESPACE
@@ -68,6 +84,7 @@ kubectl config set-context --current --namespace $NAMESPACE
 # Relax the namspace privileges so that velero can successfully backup persistent volumes.
 kubectl label namespace $NAMESPACE pod-security.kubernetes.io/enforce=privileged --overwrite
 
+# For some reason velero describe can see a backup before velero restore, or velero get.
 while ! velero get backups | grep -q "$BACKUP_NAME"; do
   echo "Waiting for backup $BACKUP_NAME..."
   sleep 5 
@@ -96,7 +113,7 @@ done
 echo Restoration status is: $RESTORE_STATUS
 
 echo
-echo "Reset the pod security levels back to original settings."
+echo "Resetting the pod security levels back to original settings."
 kubectl label namespace $NAMESPACE  pod-security.kubernetes.io/enforce=$ORIG_PRIVILAGES --overwrite
 
 SCHEDULE_NAME=$NAMESPACE-$TO_CLUSTER_ID
